532aedd45c9784c0e1a12a6b16a9577395fa0f1b
Aditya Singh <adityasingh740745@gmail.com>
2024-11-04 20:15:48 +0530

    add work component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './fonts.css';
 import Navbar from "./components/Navbar";
+import Work from "./components/Work";
 
 const App = () => {
   return (
     <div className="w-full h-screen bg-zinc-900 font-[Satoshi] text-white">
       <Navbar />
+      <Work />
     </div>
   )
 }
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,10 @@
 const Work = () => {
     return (
-        <div>
-
+        <div className="w-full">
+            <div className="max-w-screen-xl mx-auto">
+                <h1 className="text-[36rem] leading-none tracking-tight font-medium text-center select-none">work</h1>
+            </div>
+            
         </div>
     )
 }
