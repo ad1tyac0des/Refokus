b7af1631d52c93b25140f4ab05a5f9dcf3907c72
Aditya Singh <adityasingh740745@gmail.com>
2024-11-11 14:31:32 +0530

    feat: add more props to make it reusable button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,12 @@ import gsap from "gsap";
 import { useRef } from "react";
 import { IoIosReturnRight } from "react-icons/io";
 
-const Button = ({ title = "Start a Project" }) => {
+const Button = ({
+    title = "Start a Project",
+    icon = true,
+    bgColor = "bg-white",
+    textColor = "text-black",
+}) => {
     const buttonRef = useRef(null);
 
     const btnAnimation = (yOffset) => {
@@ -18,17 +23,17 @@ const Button = ({ title = "Start a Project" }) => {
             ref={buttonRef}
             onMouseEnter={() => btnAnimation(-100)}
             onMouseLeave={() => btnAnimation(0)}
-            className="button w-36 h-9 bg-white flex justify-center items-center text-sm font-light text-black rounded-full cursor-pointer z-10"
+            className={`button w-36 h-9 flex justify-center items-center border text-sm font-light rounded-full cursor-pointer z-10 ${bgColor} ${textColor}`}
         >
             <div className="frame h-[85%] overflow-hidden">
                 <div className="frame-content h-full flex items-center justify-center gap-3">
                     <span>{title}</span>
-                    <IoIosReturnRight />
+                    {icon && <IoIosReturnRight />}
                 </div>
 
                 <div className="frame-content h-full flex items-center justify-center gap-3">
                     <span>{title}</span>
-                    <IoIosReturnRight />
+                    {icon && <IoIosReturnRight />}
                 </div>
             </div>
         </div>
